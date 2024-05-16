848d64c05aa4f0ea9080f91a081e74824e1386a7
Auronmussliu1 <auron.musliu@student.uni-pr.edu>
2024-05-16 23:17:50 +0200

    shendrrimi i karaktereve ne numra dhe anasjelltas, gjithashtu padding per mesazhin

--- Kodi.py
+++ Kodi.py
@@ -10,3 +10,32 @@ matrix_rank = {
     4: [np.array([[7, 3, 19, 21], [1, 11, 15, 16], [25, 25, 22, 15], [7, 18, 12, 23]]), np.array([[18, 15, 22, 23], [17, 1, 24, 22], [24, 21, 25, 11], [9, 10, 17, 10]])]
 
 }
+
+def char_to_num(char):
+    #shendrrimi i karaktereve ne numra
+    if 'A' <= char <= 'Z': #Merr karakteret(shkronjat) prej A deri ne Z (UPPERCASE)
+        return ord(char) - ord('A') #I rendit dhe u jep vlere karaktereve SHEMBULL A=0, B=1, ... Z=25
+    elif 'a' <= char <= 'z': #Merr karakteret(shkronjat) prej a deri ne z (lowercase)
+        return ord(char) - ord('a') + 26 #I rendit dhe u jep vlere karaktereve SHEMBULL a=26, B=27, ... Z=51
+    elif '0' <= char <= '9': #Merr karakteret(numrat) prej 0 deri ne Z (UPPERCASE)
+        return ord(char) - ord('0') + 52 #I rendit dhe u jep vlere karaktereve SHEMBULL 0=52, 1=23, ... Z=61
+    else:
+        return ord('X') - ord('A')  #Per karakteret tjera e kthen "X"
+
+def num_to_char(num):
+    #Shendrrimi i numrave ne karaktere
+    #Logjika e njejt me "char_to_num" vetem se i rikthen nga numrat e caktuar ne karakteret origjinale 
+    if 0 <= num <= 25:
+        return chr(num + ord('A'))
+    elif 26 <= num <= 51:
+        return chr(num - 26 + ord('a'))
+    elif 52 <= num <= 61:
+        return chr(num - 52 + ord('0'))
+    else:
+        return None
+    
+def pad_message(message, block_size):
+    #Siguron qe mesazhi te kete padding ne menyre te duhur, ndan mesashin ne baze te inputit te userit
+    # (2, 3 ose 4) nese blloku i fundit ka karaktere me pak se inputi i userit e ben padding ate me shkronjen "A"
+    padding_length = (block_size - len(message) % block_size) % block_size
+    return message + 'A' * padding_length
